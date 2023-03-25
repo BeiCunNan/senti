539dc6b8cbf312eb39b911e7bb9aec96dd997c38
Aaron <czj1240303854@163.com>
2023-03-25 18:37:29 +0800

    Update MR、SST labels

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ def get_config():
     parser = argparse.ArgumentParser()
     num_classes = {'sst2': 2, 'sst5': 5, 'cr': 2, 'subj': 2, 'pc': 2, 'mr': 2, 'trec': 6, 'mpqa': 2}
     max_lengths = {'sst2': 53, 'sst5': 53, 'cr': 100, 'subj': 108, 'pc': 2, 'mr': 53, 'trec': 33, 'mpqa': 34}
-    query_lengths = {'sst2': 15, 'sst5': 18, 'cr': 12, 'subj': 11, 'pc': 12, 'mr': 12, 'trec': 15, 'mpqa': 2}
+    query_lengths = {'sst2': 13, 'sst5': 16, 'cr': 12, 'subj': 11, 'pc': 12, 'mr': 12, 'trec': 15, 'mpqa': 13}
 
     '''Base'''
     parser.add_argument('--data_dir', type=str, default='data')
@@ -27,7 +27,7 @@ def get_config():
                                  'text_hiddens', 'cnn+rnn', 'cls_explain', 'self_attention', 'san'])
 
     '''Optimization'''
-    parser.add_argument('--train_batch_size', type=int, default=1)
+    parser.add_argument('--train_batch_size', type=int, default=10)
     parser.add_argument('--test_batch_size', type=int, default=64)
     parser.add_argument('--num_epoch', type=int, default=10)
     parser.add_argument('--lr', type=float, default=1e-5)
--- data analysis.py
+++ data analysis.py
@@ -1,11 +1,11 @@
 import json
 import os
 
-train_data = json.load(open(os.path.join('./data/', 'SST2_Train.json'), 'r', encoding='utf-8'))
-test_data = json.load(open(os.path.join('./data/', 'SST2_Test.json'), 'r', encoding='utf-8'))
+train_data = json.load(open(os.path.join('./data/', 'MR_Train.json'), 'r', encoding='utf-8'))
+test_data = json.load(open(os.path.join('./data/', 'MR_Test.json'), 'r', encoding='utf-8'))
 sst5_label_dict = {'very negative': 'terrible', 'negative': 'bad', 'neutral': 'okay', 'positive': 'good',
                    'very positive': 'perfect'}
-sst2_label_dict = {'positive': 'good', 'negative': 'bad'}
+sst2_label_dict = {'positive': 'great', 'negative': 'terrible'}
 
 result = []
 for raw_data in test_data:
@@ -17,5 +17,5 @@ for raw_data in test_data:
     }
     result.append(item)
 
-with open('data/SST2_Test.json', 'w') as w:
+with open('data/MR_Test.json', 'w') as w:
     json.dump(result, w)
