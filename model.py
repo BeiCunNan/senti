bbb2237d7e40afdbfbb1c9f3aba327a3f92e55ee
Aaron <czj1240303854@163.com>
2023-03-25 19:49:15 +0800

    Update the whole model

--- model.py
+++ model.py
@@ -25,7 +25,193 @@ class AttentionPooling(nn.Module):
         return pooled_output
 
 
-class A(nn.Module):
+class AttentionPooling_a(nn.Module):
+    def __init__(self, input_size):
+        super(AttentionPooling, self).__init__()
+
+        self.fc = nn.Linear(input_size, 1)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, inputs):
+        # inputs: [batch_size, seq_len, input_size]
+
+        # 计算注意力权重
+        attention_weights = self.fc(inputs)
+        attention_weights = self.softmax(attention_weights)
+
+        # 对每个时间步的输出加权求和
+        pooled_output = torch.sum(attention_weights * inputs, dim=1)
+
+        return pooled_output
+
+
+class AttentionPooling_b(nn.Module):
+    def __init__(self, input_size):
+        super(AttentionPooling, self).__init__()
+
+        self.fc = nn.Linear(input_size, 1)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, inputs):
+        # inputs: [batch_size, seq_len, input_size]
+
+        # 计算注意力权重
+        attention_weights = self.fc(inputs)
+        attention_weights = self.softmax(attention_weights)
+
+        # 对每个时间步的输出加权求和
+        pooled_output = torch.sum(attention_weights * inputs, dim=1)
+
+        return pooled_output
+
+
+class AttentionPooling_c(nn.Module):
+    def __init__(self, input_size):
+        super(AttentionPooling, self).__init__()
+
+        self.fc = nn.Linear(input_size, 1)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, inputs):
+        # inputs: [batch_size, seq_len, input_size]
+
+        # 计算注意力权重
+        attention_weights = self.fc(inputs)
+        attention_weights = self.softmax(attention_weights)
+
+        # 对每个时间步的输出加权求和
+        pooled_output = torch.sum(attention_weights * inputs, dim=1)
+
+        return pooled_output
+
+
+class A_CLS(nn.Module):
+    def __init__(self, base_model, num_classes, max_lengths, query_lengths, cls_model, query_model):
+        super().__init__()
+        self.base_model = base_model
+        self.cls_model = cls_model
+        self.query_model = query_model
+        self.num_classes = num_classes
+        self.max_lengths = max_lengths
+        self.query_lengths = query_lengths
+
+        for param in base_model.parameters():
+            param.requires_grad = (True)
+
+        # Model a
+        self.akey_layer = nn.Linear(self.base_model.config.hidden_size, self.base_model.config.hidden_size)
+        self.aquery_layer = nn.Linear(self.base_model.config.hidden_size, self.base_model.config.hidden_size)
+        self.avalue_layer = nn.Linear(self.base_model.config.hidden_size, self.base_model.config.hidden_size)
+        self.a_norm_fact = 1 / math.sqrt(self.base_model.config.hidden_size)
+
+        self.af_key_layer = nn.Linear(self.max_lengths + self.query_lengths, self.max_lengths + self.query_lengths)
+        self.af_query_layer = nn.Linear(self.max_lengths + self.query_lengths, self.max_lengths + self.query_lengths)
+        self.af_value_layer = nn.Linear(self.max_lengths + self.query_lengths, self.max_lengths + self.query_lengths)
+        self.af_norm_fact = 1 / math.sqrt(self.max_lengths + self.query_lengths)
+
+        # Model b
+        self.bkey_layer = nn.Linear(self.base_model.config.hidden_size, self.base_model.config.hidden_size)
+        self.bquery_layer = nn.Linear(self.base_model.config.hidden_size, self.base_model.config.hidden_size)
+        self.bvalue_layer = nn.Linear(self.base_model.config.hidden_size, self.base_model.config.hidden_size)
+        self.b_norm_fact = 1 / math.sqrt(self.base_model.config.hidden_size)
+
+        self.bf_key_layer = nn.Linear(self.max_lengths, self.max_lengths)
+        self.bf_query_layer = nn.Linear(self.max_lengths, self.max_lengths)
+        self.bf_value_layer = nn.Linear(self.max_lengths, self.max_lengths)
+        self.bf_norm_fact = 1 / math.sqrt(self.max_lengths)
+
+        # Model c
+        self.ckey_layer = nn.Linear(self.base_model.config.hidden_size, self.base_model.config.hidden_size)
+        self.cquery_layer = nn.Linear(self.base_model.config.hidden_size, self.base_model.config.hidden_size)
+        self.cvalue_layer = nn.Linear(self.base_model.config.hidden_size, self.base_model.config.hidden_size)
+        self.c_norm_fact = 1 / math.sqrt(self.base_model.config.hidden_size)
+
+        self.cf_key_layer = nn.Linear(self.query_lengths, self.query_lengths)
+        self.cf_query_layer = nn.Linear(self.query_lengths, self.query_lengths)
+        self.cf_value_layer = nn.Linear(self.query_lengths, self.query_lengths)
+        self.cf_norm_fact = 1 / math.sqrt(self.query_lengths)
+
+        self.fnn = nn.Sequential(
+            # nn.Dropout(0.5),
+            nn.Linear(self.base_model.config.hidden_size * 9, self.base_model.config.hidden_size),
+            nn.Linear(self.base_model.config.hidden_size, num_classes)
+        )
+
+        self.A_Att_Pooling = AttentionPooling_a(self.base_model.config.hidden_size * 2)
+        self.B_Att_Pooling = AttentionPooling_b(self.base_model.config.hidden_size * 2)
+        self.C_Att_Pooling = AttentionPooling_c(self.base_model.config.hidden_size * 2)
+
+    def forward(self, inputs, inputs_cls, inputs_querys):
+        tokens = self.base_model(**inputs).last_hidden_state
+        cls_tokens = self.cls_model(**inputs_cls).last_hidden_state
+        query_tokens = self.query_model(**inputs_querys).last_hidden_state
+
+        CLS = tokens[:, 0, :]
+        cls_CLS = cls_tokens[:, 0, :]
+        query_CLS = query_tokens[:, 0, :]
+
+        tokens_padding = F.pad(tokens.permute(0, 2, 1), (0, self.max_lengths + self.query_lengths - tokens.shape[1]),
+                               mode='constant',
+                               value=0).permute(0, 2, 1)
+        cls_padding = F.pad(cls_tokens.permute(0, 2, 1), (0, self.max_lengths - tokens.shape[1]),
+                            mode='constant',
+                            value=0).permute(0, 2, 1)
+        # TSA && FSA
+        aK = self.akey_layer(tokens_padding)
+        aQ = self.aquery_layer(tokens_padding)
+        aV = self.avalue_layer(tokens_padding)
+        aattention = nn.Softmax(dim=-1)((torch.bmm(aQ, aK.permute(0, 2, 1))) * self.a_norm_fact)
+        aTSA = torch.bmm(aattention, aV)
+
+        aK_N = self.f_key_layer(tokens_padding.permute(0, 2, 1))
+        aQ_N = self.f_query_layer(tokens_padding.permute(0, 2, 1))
+        aV_N = self.f_value_layer(tokens_padding.permute(0, 2, 1))
+        aattention_N = nn.Softmax(dim=-1)((torch.bmm(aQ_N, aK_N.permute(0, 2, 1))) * self.af_norm_fact)
+        aFSA = torch.bmm(aattention_N, aV_N).permute(0, 2, 1)
+
+        # Combine T and F Method 2
+        a_TFSA = self.A_Att_Pooling(torch.cat((aTSA, aFSA), 2))
+
+        # TSA && FSA
+        bK = self.bkey_layer(cls_padding)
+        bQ = self.bquery_layer(cls_padding)
+        bV = self.bvalue_layer(cls_padding)
+        battention = nn.Softmax(dim=-1)((torch.bmm(bQ, bK.permute(0, 2, 1))) * self.b_norm_fact)
+        bTSA = torch.bmm(battention, bV)
+
+        bK_N = self.bf_key_layer(cls_padding.permute(0, 2, 1))
+        bQ_N = self.bf_query_layer(cls_padding.permute(0, 2, 1))
+        bV_N = self.bf_value_layer(cls_padding.permute(0, 2, 1))
+        battention_N = nn.Softmax(dim=-1)((torch.bmm(bQ_N, bK_N.permute(0, 2, 1))) * self.bf_norm_fact)
+        bFSA = torch.bmm(battention_N, bV_N).permute(0, 2, 1)
+
+        # Combine T and F Method 2
+        b_TFSA = self.B_Att_Pooling(torch.cat((bTSA, bFSA), 2))
+
+        # TSA && FSA
+        cK = self.ckey_layer(query_tokens)
+        cQ = self.cquery_layer(query_tokens)
+        cV = self.cvalue_layer(query_tokens)
+        cattention = nn.Softmax(dim=-1)((torch.bmm(cQ, cK.permute(0, 2, 1))) * self.c_norm_fact)
+        cTSA = torch.bmm(cattention, cV)
+
+        cK_N = self.cf_key_layer(query_tokens.permute(0, 2, 1))
+        cQ_N = self.cf_query_layer(query_tokens.permute(0, 2, 1))
+        cV_N = self.cf_value_layer(query_tokens.permute(0, 2, 1))
+        cattention_N = nn.Softmax(dim=-1)((torch.bmm(cQ_N, cK_N.permute(0, 2, 1))) * self.cf_norm_fact)
+        cFSA = torch.bmm(cattention_N, cV_N).permute(0, 2, 1)
+
+        # Combine T and F Method 2
+        c_TFSA = self.C_Att_Pooling(torch.cat((cTSA, cFSA), 2))
+
+        output_ALL = torch.cat((CLS, cls_CLS, query_CLS, a_TFSA, b_TFSA, c_TFSA), 1)
+
+        predicts = self.fnn(output_ALL)
+
+        return predicts
+
+
+class B(nn.Module):
     def __init__(self, base_model, num_classes, max_lengths, query_lengths, cls_model, query_model):
         super().__init__()
         self.base_model = base_model
@@ -62,7 +248,6 @@ class A(nn.Module):
 
         cls_outputs = self.cls_model(**inputs_cls).last_hidden_state[:, 0, :]
         query_outputs = self.query_model(**inputs_querys).last_hidden_state[:, 0, :]
-
 
         tokens_padding = F.pad(tokens.permute(0, 2, 1), (0, self.max_lengths + self.query_lengths - tokens.shape[1]),
                                mode='constant',
@@ -1229,4 +1414,3 @@ class Self_Attention_New5(nn.Module):
         predicts = self.fnn(output_ALL)
 
         return predicts
-
